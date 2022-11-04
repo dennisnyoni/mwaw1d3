91a2f7239a5e4dcf30827272f5e8e6965b397ff0
dennisnyoni <dennisnyoni@rocketmail.com>
2022-11-03 19:20:55 -0500

    co

--- express_from_http/index.js
+++ express_from_http/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+/*app.get('/', function(req, res) {
     res.sendFile('index.html');
 });
 
@@ -21,6 +21,6 @@ app.get('/page2', function(req, res) {
 
 app.post('/', function(req, res) {
     res.status(200).send(JSON.stringify({ title: "The Matrix" }));
-});
+});*/
 
 app.listen(process.env.PORT);
